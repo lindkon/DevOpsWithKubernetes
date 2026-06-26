19fe2574cc9891dbdee0604ca4d79d82309512e9
lindkon <konrad.sam.linder@gmail.com>
2026-06-26 18:11:57 +0300

    completed exercise 1.7

--- log_output/index.js
+++ log_output/index.js
@@ -1,13 +1,33 @@
-const {
-  randomUUID,
-} = require('node:crypto');
+const crypto = require('crypto');
+const http = require('http');
 
-const s = randomUUID();
+
+const s = crypto.randomUUID();
+let currentStatus = new Date().toISOString() + ": " + s;
 
 const printLog = () => {
   const timestamp = new Date().toISOString();
-  console.log(`${timestamp}: ${s}`);
+  currentStatus = `${timestamp}: ${s}`;
+  console.log(currentStatus);
 };
 
 printLog();
 setInterval(printLog, 5000);
+
+const PORT = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(currentStatus);
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`);
+});
